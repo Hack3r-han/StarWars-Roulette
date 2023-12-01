9be8fd3b0f9136a0955b97c7901337a450f8b641
Paolo Damen <paolodamen@gmail.com>
2023-12-01 09:42:59 +0100

    modal

--- modal.js
+++ modal.js
@@ -1,21 +1,27 @@
-let names = [];
+        const names = [];
         function getName() { 
         let inputName = document.querySelector("#inputName");
         let name = inputName.value
-        document.querySelector("h3").innerHTML = name;
+        document.querySelector("h4").innerHTML = name;
         names.push(name)
             inputName.value = " "
-            document.querySelector("#names").textContent = names   
+            document.querySelector("#names").textContent = names  
+            console.log(names.indexOf(name)) 
+            localStorage.setItem( "array", names)
         }
 
 
         function deleteName(){
             let inputName = document.querySelector("#inputName");
-            let name1 = inputName.value;
-            const index = names.indexOf(name1);
-            const x = names.splice(index, 1)
-            inputName.value = " "
-            document.querySelector("#names").textContent = names             
+            let name = inputName.value;
+            console.log(names)
+            console.log(name)
+            let index = names.indexOf(name)
+            console.log(index)
+            let x = names.splice(index, 1)
+
+            document.querySelector("#names").textContent = names
+            inputName.value = " "             
         }
         document.querySelector("#add").addEventListener("click", getName)
         document.querySelector("#delete").addEventListener("click", deleteName)
