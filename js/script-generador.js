b7392a2f0ac9d59a7fd6d5bb58264420687a87bf
laudyeneth <laudyeneth@hotmail.com>
2023-12-03 19:15:06 +0100

    Se añadio comentarios descriptivos a los scripts

--- js/script-generador.js
+++ js/script-generador.js
@@ -1,19 +1,21 @@
+//       *********      Array de base         *********      
+
 let nombresArray = ["Max", "Alicia","Jose","Mercedes","Jander","Yessi","Anzu","Neimy","Hanna","Isaac","Juanjo","David",
 "Paolo","Alba","Joaco","Alberto","Denisse", "Alfredo","Marcela","Laudy", "Victor","Yady","Alvaro","Roxana","Elena"];
 
-//     *********      Función para elegir nombre      **********
-
-function elegirNombreRandom(){
-    let resultado = document.getElementById("randomResultado");
-    let nombreAleatorio = obtenerNombreAleatorio(nombresArray);
-    resultado.textContent = nombreAleatorio;
-}
-
 //     *********      Función para elegir nombre  aleatorio    **********
 
+function elegirNombreRandom(){
+    let resultado = document.getElementById("randomResultado");  // Obtiene el elemento del documento con id de etiqueta p
+    let nombreAleatorio = obtenerNombreAleatorio(nombresArray); // Llama función obtenerNombreAleatorio y guarda nombre aleatorio 
+    resultado.textContent = nombreAleatorio; // Asigna  nombre aleatorio como texto del elemento con id randomResultado.
+}
+
+//     *********      Función que toma array como argumento y devuelve elemento aleatorio   **********
+
 function obtenerNombreAleatorio(array) {
-    let indiceAleatorio = Math.floor(Math.random() * array.length);
-    return array[indiceAleatorio];
+    let indiceAleatorio = Math.floor(Math.random() * array.length); // Genera un indice aleatorio dentro del rango del array
+    return array[indiceAleatorio]; // Devuelve el elemento del array (que esta en el indice aleatorio)
   }
 
 //     *********      Función para pintar el listado completo de nombresArray     **********
@@ -35,7 +37,7 @@ function obtenerNombreAleatorio(array) {
 //     *********      Función para agregar un elemento al array    **********
 
   function agregarNombre() {
-    let addElementInput = document.getElementById("nombreInput");
+    let addElementInput = document.getElementById("nombreInput"); //recibe como parametro id del input
     let newName = addElementInput.value.trim(); //Quitas espacios, adelante y atras
   
     if (newName !== "") { //si es diferente de vacio
@@ -49,7 +51,7 @@ function obtenerNombreAleatorio(array) {
   
   function eliminarNombre() {
     let removeElementInput = document.getElementById("nombreEliminar"); //parametro: id del input
-    let nameToRemove = removeElementInput.value.trim();
+    let nameToRemove = removeElementInput.value.trim(); //Quitas espacios, adelante y atras
   
     if (nameToRemove !== "") {// si es diferente de vacio
       let index = nombresArray.indexOf(nameToRemove); // devuelve la posicion de elemento
@@ -58,8 +60,9 @@ function obtenerNombreAleatorio(array) {
         nombresArray.splice(index, 1);// borra 1 elemento de esa posicion o index
         removeElementInput.value = ""; // Limpiar el input después de eliminar
         // mostrarNombreModal();//vuelve a pintar el listado
-      } else {// sino encontro el elemento
-        alert("El nombre no existe en la lista.");
+      } 
+      else {// sino encontro el elemento
+        alert("El nombre no existe en el listado");
       }
     }
   }
